9d8a0045a06f2948f23d761dc96344ab2482a14f
Vicente Burchard-Levine <48279018+VicenteBurchard@users.noreply.github.com>
2021-11-17 13:19:46 +0100

    Add files via upload

--- Main_run3SEB_ground.py
+++ Main_run3SEB_ground.py
@@ -45,8 +45,15 @@ G_santanello = [0.25, 3.0, 24.0]
 #==============================================================================
 
 #constant/static parameters of different sites
-site_params = {1: {'site_name':'ES-LM1', 'lat': 39.945592, 'lon':-5.782977, 'stdlon':-15.0, 'z_u': 15, 'z_t': 15,
-                   'f_c': 0.20 , 'f_c_sub': 1.0 ,'h_c': 8.7, 'h_c_sub':0.5, 'leaf_width': 0.05, 'leaf_width_sub': 0.01}}
+site_params = {1: {'site_name':'AU-Dry', 'lat': -15.2588, 'lon':132.3706, 'stdlon':135.0, 'z_u': 15, 'z_t': 15,
+                   'f_c': 0.25 , 'f_c_sub': 1.0 ,'h_c': 12.3, 'h_c_sub':1., 'leaf_width': 0.05, 'leaf_width_sub': 0.01},
+               2: {'site_name':'ES-LM1', 'lat': 39.945592, 'lon':-5.782977, 'stdlon':-15.0, 'z_u': 15, 'z_t': 15,
+                   'f_c': 0.20 , 'f_c_sub': 1.0 ,'h_c': 8.7, 'h_c_sub':0.5, 'leaf_width': 0.05, 'leaf_width_sub': 0.01},
+               3: {'site_name':'US-Ton', 'lat': 38.4311 , 'lon':-120.966 , 'stdlon':-120.0, 'z_u': 23.5, 'z_t': 23.5,
+                   'f_c': 0.48 , 'f_c_sub': 1.0 ,'h_c': 9.4, 'h_c_sub':0.5, 'leaf_width': 0.05, 'leaf_width_sub': 0.01},
+               4: {'site_name':'ES-Abr', 'lat': 38.701839, 'lon':-6.785881, 'stdlon':-15.0, 'z_u': 12, 'z_t': 12,
+                   'f_c': 0.24 , 'f_c_sub': 1.0 ,'h_c': 6.6, 'h_c_sub':0.5, 'leaf_width': 0.05, 'leaf_width_sub': 0.01},
+              }
 
 #==============================================================================
 # Canopy and Soil spectra
@@ -76,13 +83,25 @@ outputTxtFieldNames = ['site_ID', 'site_name', 'year','hydro_year', 'DOY', 'time
                        'zo','d', 'zo_sub', 'd_sub', 'G_model', 'R_s','R_sub', 'R_x', 'R_a', 'u_friction', 'L']
 
 
-#set temporal period and sites (in hydro years)
-## Simulation period:
-### ES-LM1: 2015-2018
-years = [2015, 2016, 2017, 2018]
+#==============================================================================
+# Run 3SEB
+#==============================================================================
 
-###### 1: ES-LM1 ######
-sites=[1]
+#set which sites to process
+
+###### 1: AU-Dry, 2: ES-LM1, 3: US-Ton, 4: ES-Abr ######
+#default: ES-LM1
+sites=[2]
+
+#set years to process
+
+#simulation periods
+### AU-Dry: 2012-2015
+### ES-LM1: 2015-2018
+### US-Ton: 2015-2019
+### ES-Abr: 2016-2019
+#default: ES-LM1 (all years)
+years =  [2015, 2016, 2017, 2018]
 
 #run 3SEB for specified years and sites
 for year in years:
@@ -143,10 +162,6 @@ for year in years:
         #Fg
         fg_grass_daily = MODIS_LAI['fg_grass']
         fg_tree_daily = MODIS_LAI['fg_tree']
-        #correcting for tree influence on grass fg
-        fg_grass_daily[fg_tree_daily==0.9] = (fg_grass_daily[fg_tree_daily==0.9] - (fC_value*fg_tree_daily[fg_tree_daily==0.9]))/(1-fC_value)
-        fg_grass_daily[fg_grass_daily<0] = 0
-        fg_grass_daily[fg_grass_daily > 1] = 1
 
         #create output directory for site
         outdir_site = join(outdir,site_name)
@@ -158,7 +173,6 @@ for year in years:
         metdata = pd.read_csv(meteoFile, sep='\t')
         # filter for year in question
         metdata = metdata[metdata['year_hydro'] == year]
-
 
         #get dates at half hourly time step
         dates_str = pd.to_datetime(metdata['Date'].values).strftime('%Y-%m-%d')
@@ -193,7 +207,7 @@ for year in years:
             fg_C_array[validData_met] = fg_tree_daily[validData_veg]
 
         #set minimum LAI for understory/grass LAI
-        min_lai = 0.35
+        min_lai = 0.5
         LAI_grass[LAI_grass < min_lai] = min_lai
 
         ############# initialize vegetation structural parameters ################
@@ -202,9 +216,10 @@ for year in years:
         hC = np.ones(LAI_tree.shape) * hC_value # tree height
 
         #hC_sub, grass canopy height
-        #grass vegetation depends on LAI (like Sen-ET, Guzinski et al. 2020)
-        hc_factor = np.minimum((LAI_grass/2.)**3, 1)
-        hC_sub = 0.2*hC_sub_value + 0.8*(hC_sub_value*hc_factor)
+        #grass height depends on LAI (scaled using a power function --> like Sen-ET, see Guzinski et al. (2020): https://doi.org/10.3390/rs12091433)
+        LAI_grass_max = np.nanmax(LAI_grass)
+        hc_factor = np.minimum((LAI_grass/LAI_grass_max)**2, 1)
+        hC_sub = 0.3*hC_sub_value + 0.7*(hC_sub_value*hc_factor)
 
         #wC, tree canopy height to width ratio
         wC = np.ones(LAI_tree.shape) * wC_value
@@ -303,12 +318,9 @@ for year in years:
                                                    np.full_like(LAI_tree, spectraGrd['rsoiln']), hC, hb, wC,fC,
                                                    x_LAD=x_LAD_C, x_LAD_sub=x_LAD_sub, LAI_eff=LAI_tree_eff, LAI_eff_sub=LAI_grass_eff)
 
-
-
         Rn_sw_C[~np.isfinite(Rn_sw_C)] = 0
         Sn_C_sub[~np.isfinite(Sn_C_sub)] = 0
         Sn_S[~np.isfinite(Sn_S)] = 0
-
 
         #Run 3SEB
         [flag_PT_all, T_S, T_C, T_C_sub, T_AC, L_n_sub, L_nC, Ln_C_sub, Ln_S,
@@ -421,8 +433,10 @@ for year in years:
             return rmse, cor, bias
 
 
-        #plot half-hourly modelled vs observed flux scatter plot
-        mask = np.logical_and.reduce((flag_PT_all < 5, metdata['SW_in'].values > 25, Sn_PT > 25))
+        #plot half-hourly modelled vs observed flux scatter
+        noneg = np.logical_and.reduce((H_PT >-50,H_obs>-50, LE_PT>-50,LE_obs>-50,  LE_PT<1500, LE_obs<1500, H_PT<1500, H_obs<1500 ))
+        QC = np.logical_and.reduce((flag_PT_all < 5, metdata['SW_in'].values > 25, Sn_PT > 25))
+        mask = np.logical_and(noneg, QC )
         plt.figure()
         plt.scatter(H_PT[mask], H_obs[mask], c='r', marker='.', alpha=0.2, label='H', s=3)
         plt.scatter(LE_PT[mask], LE_obs[mask], color='b', marker='.', alpha=0.2, label='LE', s=3)
@@ -444,7 +458,7 @@ for year in years:
         for lh in leg.legendHandles:
             lh.set_alpha(1)
 
-        plt.savefig(join(outdir_site, 'FluxPartitioning_%s_%s_%s_Site%s.png' % (res_string, G_string, year, site )))
+        plt.savefig(join(outdir_site, 'FluxPartitioning_%s_%s_%s_%s.png' % (res_string, G_string, year, site_name )))
         plt.close()
 
 
@@ -465,5 +479,5 @@ for year in years:
             int(rmse_LE), int(bias_LE), np.round(cor_LE, 2)),
                     backgroundcolor='white', linespacing=1.15, family='monospace')
 
-        plt.savefig(join(outdir_site, 'AvailableEnergy_vs_obs_%s_%s_%s_Site%s.png' % (res_string, G_string, year, site)))
+        plt.savefig(join(outdir_site, 'AvailableEnergy_vs_obs_%s_%s_%s_%s.png' % (res_string, G_string, year, site_name)))
         plt.close()
